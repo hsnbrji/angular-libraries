675bec3daf720ef84a049fb1dfffff306df8b859
Hassan Bourji <Hassan.bourji@pin-pay.com>
2020-01-08 12:54:25 +0200

    fix: component export

--- projects/ionic4-autocomplete/src/public-api.ts
+++ projects/ionic4-autocomplete/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ionic4-autocomplete
  */
 
-export * from './lib/ionic4-autocomplete.service';
-export * from './lib/ionic4-autocomplete.component';
+export * from './lib/auto-complete.component';
 export * from './lib/autocomplete.module';
