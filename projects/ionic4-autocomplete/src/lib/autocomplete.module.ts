b2b139635d71d563d5db49c25751370f4c0f7211
Hassan Bourji <Hassan.bourji@pin-pay.com>
2020-01-08 12:54:39 +0200

    chore: imported modules

--- projects/ionic4-autocomplete/src/lib/autocomplete.module.ts
+++ projects/ionic4-autocomplete/src/lib/autocomplete.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import {AutoCompleteComponent} from './auto-complete.component';
-
+import {IonicModule} from '@ionic/angular';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [AutoCompleteComponent],
   imports: [
+    IonicModule.forRoot(),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [AutoCompleteComponent]
 })
