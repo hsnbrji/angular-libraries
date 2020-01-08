5612878daecd79106ba4b6334453f7ef20384bdb
Hassan Bourji <Hassan.bourji@pin-pay.com>
2020-01-08 12:30:13 +0200

    feat: created library

---
+++ projects/ionic4-autocomplete/src/lib/autocomplete.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {AutoCompleteComponent} from './auto-complete.component';
+
+
+
+@NgModule({
+  declarations: [AutoCompleteComponent],
+  imports: [
+  ],
+  exports: [AutoCompleteComponent]
+})
+export class AutocompleteModule { }
---
+++ projects/ionic4-autocomplete/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ionic4-autocomplete
+ */
+
+export * from './lib/ionic4-autocomplete.service';
+export * from './lib/ionic4-autocomplete.component';
+export * from './lib/autocomplete.module';
